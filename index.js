fc2b721c98e2aca8662f13c69f9264602285f850
Hai567 <79144082+Hai567@users.noreply.github.com>
2022-08-29 12:04:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ for (let i = 0; i < 7; i++) {
       let keyInnerHTML = this.innerHTML;
       switch (keyInnerHTML) {
         case "a": new Audio ("sounds/crash.mp3").play()
-        animation(event.key)
+        animation(keyInnerHTML)
         break;
         case "s": new Audio ("sounds/kick-bass.mp3").play()
-        animation(event.key)
+        animation(keyInnerHTML)
         break;
         case "d": new Audio ("sounds/snare.mp3").play()
-        animation(event.key)
+        animation(keyInnerHTML)
         break;
         case "f": new Audio ("sounds/tom-1.mp3").play()
-        animation(event.key)
+        animation(keyInnerHTML)
         break;
         case "g": new Audio ("sounds/tom-2.mp3").play()
-        animation(event.key)
+        animation(keyInnerHTML)
         break;
         case "h": new Audio ("sounds/tom-3.mp3").play()
-        animation(event.key)
+        animation(keyInnerHTML)
         break;
         case "j": new Audio ("sounds/tom-4.mp3").play()
-        animation(event.key)
+        animation(keyInnerHTML)
         break;
           
-        default: console.log(event.key)
+        default: console.log(keyInnerHTML)
       }
     });
 }
